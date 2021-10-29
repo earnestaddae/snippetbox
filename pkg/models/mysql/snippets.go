e0354971f6b6b00f78afeab81071828474ad73cb
Ernest Addae <earnestaddae@gmail.com>
2021-10-29 13:51:53 +0800

    Insert new and retrieve record

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,55 @@
+package mysql
+
+import (
+	"database/sql"
+	"errors"
+
+	"earnestaddae.me/snippetbox/pkg/models"
+)
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert inserts a new snippet into the database
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
+}
+
+// Get gets a single snippet from the database based on its id
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
+}
+
+// Latest returns the 10 most recent snippets
+func (m *SnippetModel) Latest() ([]models.Snippet, error) {
+	return nil, nil
+}
