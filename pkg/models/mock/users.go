cde4bb7476ff5292c853a23c84f03b88f9a38107
Ernest Addae <earnestaddae@gmail.com>
2021-11-01 17:51:06 +0800

    Create mock test and add interface to snippets and users field in the application struct

---
+++ pkg/models/mock/users.go
@@ -0,0 +1,44 @@
+package mock
+
+import (
+	"time"
+
+	"earnestaddae.me/snippetbox/pkg/models"
+)
+
+var mockUser = &models.User{
+	ID:      1,
+	Name:    "Alice",
+	Email:   "alice@wonderland.com",
+	Created: time.Now(),
+	Active:  true,
+}
+
+type UserModel struct{}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	switch email {
+	case "dupe@wonderland.com":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	switch email {
+	case "alice@wonderland.com":
+		return 1, nil
+	default:
+		return 0, models.ErrInvalidCredentials
+	}
+}
+
+func (m *UserModel) Get(id int) (*models.User, error) {
+	switch id {
+	case 1:
+		return mockUser, nil
+	default:
+		return nil, models.ErrNoRecord
+	}
+}
